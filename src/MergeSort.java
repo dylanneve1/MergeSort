55e2c4216a143f7a391a9a0a1b1aea6d76a1cd4b
dylanneve1 <dylanneve1@gmail.com>
2018-07-31 11:20:48 +0100

    Merge and split functions

--- src/MergeSort.java
+++ src/MergeSort.java
@@ -1,3 +1,4 @@
+import java.util.Arrays;
 
 public class MergeSort {
 
@@ -22,12 +23,37 @@ public class MergeSort {
 	}
 	
 	public static void splitList(int numbers[], int numbersLeftSide[], int numbersRightSide[]) {
-		
+
+		int n = numbers.length;
+
+		int[] a = new int[(n + 1)/2];
+		int[] b = new int[n - a.length];
+
+		for (int i = 0; i < n; i++)
+		{
+			if (i < a.length)
+				a[i] = numbers[i];
+			else
+				b[i - a.length] = numbers[i];
+		}
+
+		System.out.println(Arrays.toString(a));
+		System.out.println(Arrays.toString(b));
 	}
 	
 	public static int[] mergeList(int numbersLeftSide[], int numbersRightSide[]) {
 		
-		int newList[] = {0};
-		return newList;
+		int[]c = new int[numbersLeftSide.length + numbersRightSide.length];
+		int count = 0;
+		
+		for(int i = 0; i < numbersLeftSide.length; i++) { 
+			c[i] = numbersLeftSide[i];
+	        count++;
+	    } 
+	    for(int j = 0;j < numbersRightSide.length;j++) { 
+	        c[count++] = numbersRightSide[j];
+	    } 
+	    for(int i = 0; i < c.length;i++) System.out.print(c[i] + " ");
+		return c;
 	}
 }

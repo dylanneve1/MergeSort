8d108f3f0658a0851de0aeec441aad180ff5491d
dylanneve1 <dylanneve1@gmail.com>
2018-07-31 10:55:54 +0100

    Add splitList and mergeList functions

--- src/MergeSort.java
+++ src/MergeSort.java
@@ -2,16 +2,32 @@
 public class MergeSort {
 
 	static int numbers[] = {5, 3, 7, 6, 9, 1, 2, 7};
+	static int numbersLeftSide[];
+	static int numbersRightSide[];
 	
 	public static void main(String[] args) {
 		mSort(numbers);
 	}
 
-	public static void mSort(int numbers[]) {
+	public static int[] mSort(int numbers[]) {
 		// If array is only 1 number exit because it is not possible to split.
 		if(numbers.length == 1) {
-			return;
+			return numbers;
 		}
 		// TODO code rest of algorithm.
+		splitList(numbers, numbersLeftSide, numbersRightSide);
+		numbersLeftSide = mSort(numbersLeftSide);
+		numbersRightSide = mSort(numbersRightSide);
+		return mergeList(numbersLeftSide, numbersRightSide);
+	}
+	
+	public static void splitList(int numbers[], int numbersLeftSide[], int numbersRightSide[]) {
+		
+	}
+	
+	public static int[] mergeList(int numbersLeftSide[], int numbersRightSide[]) {
+		
+		int newList[] = {0};
+		return newList;
 	}
 }
